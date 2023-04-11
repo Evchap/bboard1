e60902afd3448204fbd5e5e5bbd7d55250396ee6
Evgeny <chapayevkrim@rambler.ru>
2023-04-11 09:10:01 +0300

    Created application main, created home page

--- bboard1/bboard1/bboard1/settings.py
+++ bboard1/bboard1/bboard1/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #own
+    'main.apps.MainConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
