2e5e4364b4c3237f122bd628652c11de37b27e64
Evgeny <chapayevkrim@rambler.ru>
2023-04-11 10:56:05 +0300

    Created login page

--- bboard1/bboard1/main/urls.py
+++ bboard1/bboard1/main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import index
-from .views import other_page
+from .views import other_page, BBLoginView
 
 app_name = 'main'
 
 urlpatterns = [
-path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- bboard1/bboard1/main/views.py
+++ bboard1/bboard1/main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 def index(request):
     return render(request, 'main/index.html')
 
@@ -12,3 +14,6 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
