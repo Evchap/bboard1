24e97e88d4139c77dea6adbb02ffa46e0615fe6b
Evgeny <chapayevkrim@rambler.ru>
2023-04-12 07:18:14 +0300

    Created user activation page

--- bboard1/bboard1/main/urls.py
+++ bboard1/bboard1/main/urls.py
@@ -4,11 +4,12 @@ from .views import index, profile
 from .views import other_page, BBLoginView, BBLogoutView, BBPasswordChangeView
 from .views import ChangeUserInfoView
 from .views import RegisterUserView, RegisterDoneView
-
+from .views import user_activate
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/register/activate/<str:sign>/', user_activate, name='register_activate'),
     path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
     path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
--- bboard1/bboard1/main/views.py
+++ bboard1/bboard1/main/views.py
@@ -76,3 +76,25 @@ class RegisterDoneView(TemplateView):
     template_name = 'main/register_done.html'
 
 
+from django.core.signing import BadSignature
+
+from .utilities import signer
+
+def user_activate(request, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(request, 'main/bad_signature.html')
+    user = get_object_or_404(AdvUser, username=username)
+    if user.is_activated:
+        template = 'main/user_is_activated.html'
+    else:
+        template = 'main/activation_done.html'
+        user.is_active = True
+        user.is_activated = True
+        user.save()
+    return render(request, template)
+
+
+
+
